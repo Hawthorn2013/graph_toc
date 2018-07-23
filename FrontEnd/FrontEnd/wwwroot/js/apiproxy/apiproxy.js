a62f413423d854ca93a53effcf14ffb089bdc351
Hawthorn2013 <hawthorn7dd@hotmail.com>
2018-07-23 14:47:56 +0800

    Add Users And Groups Entities

--- FrontEnd/FrontEnd/wwwroot/js/apiproxy/apiproxy.js
+++ FrontEnd/FrontEnd/wwwroot/js/apiproxy/apiproxy.js
@@ -1,16 +1,26 @@
 var apiproxy = (function() {
+    var entities = {
+        Users: {
+            url: "https://developer.microsoft.com/en-us/graph/docs/concepts/azuread-users-concept-overview",
+            relation_entities: ["Documents", "Sites", "Tasks", "Meetings", "Contacts", "Email", "Calendar",],
+        },
+        Groups: {
+            url: "https://developer.microsoft.com/en-us/graph/docs/concepts/office365-groups-concept-overview",
+            relation_entities: ["Files", "Notes", "Tasks", "Sites", "Conversations", "Calendar",],
+        },
+    };
+
+    var entitiesList = [];
+    for (var entity in entities) {
+        entitiesList.push(entity);
+    }
+
     var getEntities = function() {
-        var entities = ["User", "Files", "Messages", "People", "Devices", "Coworkers", "Insights", "Chats",];
-        return entities;
+        return entitiesList;
     };
     
     var getEntity = function (entityName) {
-        var entity = {
-            url: "https://developer.microsoft.com/en-us/graph/docs/concepts/overview",
-            relation_entities: ["Files", "Messages", "People", "Devices", "Coworkers", "Insights", "Chats",],
-        };
-        if (entityName == "User") return entity;
-        else return {};
+        return entities[entityName];
     };
 
     return {
