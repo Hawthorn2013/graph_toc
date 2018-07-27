06929ca1942522f1625779ba8c3e8703f9b9466c
yazhao <yazhao@microsoft.com>
2018-07-27 19:11:39 +0800

    add edge click func

--- FrontEnd/FrontEnd/wwwroot/js/wikipedia-map/bindings.js
+++ FrontEnd/FrontEnd/wwwroot/js/wikipedia-map/bindings.js
@@ -1,120 +1,272 @@
 // This script contains (most of) the code that binds actions to events.
 
+var nowClickEdge = "";
+var nowClickEdgePathIndex = 0;
+var lastChoosePath = [];
 
 //Functions that will be used as bindings
-function expandEvent (params) { // Expand a node (with event handler)
-  if (params.nodes.length) { //Did the click occur on a node?
-      var nodeId = params.nodes[0]; //The id of the node clicked
-      //if (!nodes.get(nodeId).isExpand) {
-          //nodes.update({ id: nodeId, isExpand: true });
-      if (!nodes.get(nodeId).isMethod) {
-          expandNode(nodeId);
-      } 
-      //// open 
-      //var id = nodes.get(nodeId).id;
-      //var url;
-      //if (nodes.get(nodeId).isMethod) {
-      //    url = nodes.get(nodeId).url;
-      //} else {
-      //    url = apiproxy.getEntity(id)["url"];
-      //}
+function expandEvent(params) { // Expand a node (with event handler)
+    if (params.nodes.length) { //Did the click occur on a node?
+        var nodeId = params.nodes[0]; //The id of the node clicked
+        //if (!nodes.get(nodeId).isExpand) {
+        //nodes.update({ id: nodeId, isExpand: true });
+        console.log("Node click");
+        //console.log(nodes.get(nodeId));
+        if (!nodes.get(nodeId).isMethod) {
+            expandNode(nodeId);
+        }
+        //// open 
+        //var id = nodes.get(nodeId).id;
+        //var url;
+        //if (nodes.get(nodeId).isMethod) {
+        //    url = nodes.get(nodeId).url;
+        //} else {
+        //    url = apiproxy.getEntity(id)["url"];
+        //}
 
-      //console.log("url----->" + url);
-      //window.open(url, '_blank');
-          
-      //} else {
-         // nodes.update({ id: nodeId, isExpand: false });
-          //console.log('remove');
-     // }
-    
-  }
+        //console.log("url----->" + url);
+        //window.open(url, '_blank');
+
+        //} else {
+        // nodes.update({ id: nodeId, isExpand: false });
+        //console.log('remove');
+        // }
+
+    }
 }
 
 
-function mobileTraceEvent (params) { // Trace back a node (with event handler)
-  if (params.nodes.length) { //Was the click on a node?
-    //The node clicked
-    var page = params.nodes[0];
-    //Highlight in blue all nodes tracing back to central node
-      traceBack(page);
-      console.log("ttt");
-  } else {
-    resetProperties();
-  }
+function mobileTraceEvent(params) { // Trace back a node (with event handler)
+    if (params.nodes.length) { //Was the click on a node?
+        //The node clicked
+        var page = params.nodes[0];
+        //Highlight in blue all nodes tracing back to central node
+        traceBack(page);
+    } else {
+        resetProperties();
+    }
 }
 
-function openPageEvent (params) {
-  if (params.nodes.length) {
-    var nodeid = params.nodes[0];
-      var id = nodes.get(nodeid).id;
-      var url;
-      if (nodes.get(nodeid).isMethod) {
-          url = nodes.get(nodeid).url;
-      } else {
-          url = apiproxy.getEntity(id)["url"];
-      }
-      
-      console.log("url----->" + url);
-    window.open(url, '_blank');
-  }
+function openPageEvent(params) {
+    if (params.nodes.length) {
+        var nodeid = params.nodes[0];
+        var id = nodes.get(nodeid).id;
+        var url;
+        if (nodes.get(nodeid).isMethod) {
+            url = nodes.get(nodeid).url;
+        } else {
+            url = apiproxy.getEntity(id)["url"];
+        }
+
+        console.log("url----->" + url);
+        window.open(url, '_blank');
+    }
 }
 
 // Bind the network events
-function bindNetwork(){
-  if (isTouchDevice) { // Device has touchscreen
-    network.on("hold", expandEvent); // Long press to expand
-    network.on("click", mobileTraceEvent); // Highlight traceback on click
-  } else { // Device does not have touchscreen
-    network.on("click", expandEvent); // Expand on click
-    network.on("hoverNode", function(params){ // Highlight traceback on hover
-        traceBack(params.node);
-    });
-    network.on("blurNode", resetProperties); // un-traceback on un-hover
-  }
+function bindNetwork() {
+    if (isTouchDevice) { // Device has touchscreen
+        network.on("hold", expandEvent); // Long press to expand
+        //network.on("selectNode", mobileTraceEvent); // Highlight traceback on click
+    } else { // Device does not have touchscreen
+        network.on("click", onCilck);
+        //network.on("selectNode", expandEvent); // Expand on click
+        network.on("hoverNode", function (params) { // Highlight traceback on hover
+            traceBack(params.node);
+        });
+        network.on("blurNode", resetProperties); // un-traceback on un-hover
+    }
 
-  //Bind double-click to open page
+    //Bind double-click to open page
     network.on("doubleClick", openPageEvent);
-    network.on("selectEdge", function (params) {
-        console.log(params);
-    });
+    //network.on("selectEdge", function (params) {
+    //    edgeTraceBack(params);
+    //});
+}
+
+function onCilck(params) {
+    //console.log(params);
+    unHighlightPath(lastChoosePath);
+    if (params.nodes.length != 0) {
+        expandEvent(params);
+        return;
+    }
+    if (params.edges.length != 0) {
+        edgeTraceBack(params);
+        return;
+    }
+    
+
+}
+
+// show the api on diffient paths
+function edgeTraceBack(params) {
+    console.log("Edge click");
+    //console.log(edges.get(params.edges[0]));
+    var edgeId = params.edges[0];
+    var paths = getAPIPaths(edgeId);
+    var startNode = [edges.get(edgeId).from];
+    // add start node
+    for (var i = 0; i < paths.length; i++) {
+        paths[i] = startNode.concat(paths[i]);
+    }
+    console.log(paths);
+    if (nowClickEdge == edgeId) {
+        //console.log('next');
+        //paths = getAPIPaths();
+        if (nowClickEdgePathIndex == paths.length) {
+            nowClickEdgePathIndex = 0;
+        }
+        //unHighlightPath(lastChoosePath);
+        highlightPath(paths[nowClickEdgePathIndex]);
+        showApi(paths[nowClickEdgePathIndex]);
+        lastChoosePath = paths[nowClickEdgePathIndex];
+        nowClickEdgePathIndex = nowClickEdgePathIndex + 1;
+    } else {
+        nowClickEdge = edgeId;
+        nowClickEdgePathIndex = 0;
+        //unHighlightPath(lastChoosePath);
+        highlightPath(paths[nowClickEdgePathIndex]);
+        showApi(paths[nowClickEdgePathIndex]);
+        lastChoosePath = paths[nowClickEdgePathIndex];
+        nowClickEdgePathIndex = nowClickEdgePathIndex + 1;
+    }
+
+}
+
+function showApi(path) {
+    var apiLog = document.getElementById('apiLog');
+    var apiUrl = "https://graph.microsoft.coml";
+    for (var i = 0; i < path.length -1; i++) {
+        if (i % 2 == 0) {
+            apiUrl = apiUrl + '/' + path[i];
+        }
+    }
+    apiLog.innerHTML = apiUrl;
+}
+// highlight path
+function highlightPath(path) {
+    for (var i = 0; i < path.length; i++) {
+        if (i % 2 == 0) {
+            //console.log(i);
+            colorNode(nodes.get(path[i]), 1);
+        } else {
+            // first line need thin
+            if (i == 1) {
+                colorEdge(edges.get(path[i]), 1, 1);
+            } else {
+                colorEdge(edges.get(path[i]), 1, 3);
+            }
+        }
+    }
+}
+
+// un highlight path
+
+function unHighlightPath(path) {
+    for (var i = 0; i < path.length; i++) {
+        if (i % 2 == 0) {
+            //console.log(i);
+            colorNode(nodes.get(path[i]), 0);
+        } else {
+           
+            colorEdge(edges.get(path[i]), 0);
+        }
+    }
+}
+
+function colorEdge(es, color, width) {
+    if (color) {
+        es.width = width;
+        es.color = "#FF0000"
+    } else {
+        es.width = width;
+        es.color = "#03A9F4"
+    }
+    edges.update(es);
+}
+
+// Color node from a list based on their level. If color=1, highlight color will be used.
+function colorNode(ns, color) {
+    var colorFunc
+    if (ns.isDefault) {
+        colorFunc = color ? getYellowColor : getDefaultColor;
+    } else {
+        if (ns.isAim) {
+            colorFunc = color ? getYellowColor : getAimColor;
+        } else {
+            if (ns.isMethod) {
+                colorFunc = color ? getYellowColor : getMethodColor;
+            } else {
+                colorFunc = color ? getYellowColor : getColor;
+            }
+        }
+    }
+    ns.color = colorFunc(ns.level);
+    delete ns.x;
+    delete ns.y;
+    nodes.update(ns);
+}
+
+// get all path to edge
+function getAPIPaths(edgeId) {
+    var daId = apiproxy.getDefaultEntity()['id'];
+    if (edges.get(edgeId).from == daId) {
+        var paths = [[edgeId, daId]];
+        return paths;
+    } else {
+        var nodeId = edges.get(edgeId).from;
+        var node = nodes.get(nodeId);
+        var parents = node.parents;
+        var tempPath = [edgeId, nodeId];
+        var paths = [];
+        for (var i = 0; i < parents.length; i++) {
+            var prePaths = getAPIPaths(edges.get(getEdgeId(parents[i], nodeId)).id);
+            for (var j = 0; j < prePaths.length; j++) {
+                //console.log(tempPath.concat(prePaths[j]));
+                paths.push(tempPath.concat(prePaths[j]));
+            }
+        }
+        return paths;
+    }
 }
 
 function bind() {
 
-  // Prevent iOS scrolling
-  document.ontouchmove = function(event){
-    event.preventDefault();
-  };
+    // Prevent iOS scrolling
+    document.ontouchmove = function (event) {
+        event.preventDefault();
+    };
 
-  // Bind actions for search component.
+    // Bind actions for search component.
 
-  var cf = document.getElementsByClassName("commafield")[0];
-  //Bind the action of pressing the button
-  var submitButton = document.getElementById('submit');
-  submitButton.onclick = function() {
-    shepherd.cancel(); // Dismiss the tour if it is in progress
-    resetNetworkFromInput();
-  };
+    var cf = document.getElementsByClassName("commafield")[0];
+    //Bind the action of pressing the button
+    var submitButton = document.getElementById('submit');
+    submitButton.onclick = function () {
+        shepherd.cancel(); // Dismiss the tour if it is in progress
+        resetNetworkFromInput();
+    };
 
-  var randomButton = document.getElementById('random');
-  randomButton.onclick = randomReset;
+    var randomButton = document.getElementById('random');
+    randomButton.onclick = randomReset;
 
-  //start
-  //var tourbtn = document.getElementById("tourinit");
-  //tourbtn.onclick = function(){shepherd.start();};
-  //init default
-  resetNetworkFromInputDefault();
+    //start
+    //var tourbtn = document.getElementById("tourinit");
+    //tourbtn.onclick = function(){shepherd.start();};
+    //init default
+    resetNetworkFromInputDefault();
 
-  // Bind GitHub button
-  var ghbutton = document.getElementById("github");
-  ghbutton.onclick = function(event) {
-      window.open("https://github.com/docascode/graph_toc");
-  };
+    // Bind GitHub button
+    var ghbutton = document.getElementById("github");
+    ghbutton.onclick = function (event) {
+        window.open("https://github.com/docascode/graph_toc");
+    };
 
-  // Bind twitter button
-  var sharebutton = document.getElementById("share");
-  var buttons = document.getElementById("buttons");
-  sharebutton.onclick = function(event) {
+    // Bind twitter button
+    var sharebutton = document.getElementById("share");
+    var buttons = document.getElementById("buttons");
+    sharebutton.onclick = function (event) {
 
-  };
+    };
 }
--- FrontEnd/FrontEnd/wwwroot/js/wikipedia-map/main.js
+++ FrontEnd/FrontEnd/wwwroot/js/wikipedia-map/main.js
@@ -11,31 +11,32 @@ var needsreset = true;
 var container = document.getElementById('container');
 //Global options
 var options = {
-  nodes: {
-    shape: 'dot',
-    scaling: { min: 20,max: 30,
-      label: { min: 14, max: 30, drawThreshold: 9, maxVisible: 20 }
+    nodes: {
+        shape: 'dot',
+        scaling: {
+            min: 20, max: 30,
+            label: { min: 14, max: 30, drawThreshold: 9, maxVisible: 20 }
+        },
+        font: { size: 14, face: 'Helvetica Neue, Helvetica, Arial' }
     },
-    font: {size: 14, face: 'Helvetica Neue, Helvetica, Arial'}
-  },
-  interaction: {
-    hover: true,
-    hoverConnectedEdges: false,
-    selectConnectedEdges: true,
-  },
+    interaction: {
+        hover: true,
+        hoverConnectedEdges: false,
+        selectConnectedEdges: true,
+    },
 };
 
 var nodes = new vis.DataSet();
 var edges = new vis.DataSet();
-var data = {nodes:nodes,edges:edges};
+var data = { nodes: nodes, edges: edges };
 var initialized = false;
 
 
 //Make the network
 function makeNetwork() {
-  network = new vis.Network(container,data,options);
-  bindNetwork();
-  initialized=true;
+    network = new vis.Network(container, data, options);
+    bindNetwork();
+    initialized = true;
 }
 
 
@@ -45,116 +46,179 @@ function resetNetwork(start, name) {
     document.getElementById("submit").innerHTML = '<i class="icon ion-refresh"> </i>';
     var startID = getNeutralId(start);
     var paths = apiproxy.getPaths(startID);
-    console.log(paths);
+    //console.log(paths);
     startpages = []; // Register the page as an origin node
     tracenodes = [];
     traceedges = [];
     nodes = new vis.DataSet();
     edges = new vis.DataSet();
-
-    for (var i = 0; i < paths.length;i++) {
+    for (var i = 0; i < paths.length; i++) {
         var lastNode = "";
         for (var j = 0; j < paths[i].length; j++) {
-            var ndId = paths[i][j];
-            var ndName = apiproxy.getEntity(ndId)['name'];
-            var isAim = (ndId == startID);
-            if (lastNode == "") {
-                if (nodes.getIds().indexOf(ndId) == -1) {
-                    startpages.push(ndId)
-                    nodes.add([
-                        {
-                            id: ndId, label: wordwrap(decodeURIComponent(ndName), 20), value: 2, level: 0,
-                            color: getDefaultColor(0), x: 0, y: 0, fixed: true,parent: ndId, isDefault: true, isAim: isAim, isMethod: false
-                        } // Parent is self
-                    ]);
-                }
-               lastNode = ndId;
-            } else {
-                startpages.push(ndId)
-                if (nodes.getIds().indexOf(ndId) == -1) {
-                    startpages.push(ndId)
-                    if (isAim) {
-                        nodes.add([
-                            {
-                                id: ndId, label: wordwrap(decodeURIComponent(ndName), 20), value: 2, level: 4,
-                                color: getAimColor(0), x: 300, y: 0, fixed: true,parent: lastNode, isDefault: false, isAim: true, isMethod: false
-                            } // Parent is self
-                        ]);
-                    } else {
-                        nodes.add([
-                            {
-                                id: ndId, label: wordwrap(decodeURIComponent(ndName), 20), value: 2, level: 4,
-                                color: getColor(0), x: 0, y: 0, parent: lastNode, isDefault: false, isAim: false, isMethod: false
-                            } // Parent is self
-                        ]);
-                    }
-                   
-                }
-                if (!getEdgeConnecting(lastNode, ndId) && lastNode!=ndId) { //Don't create duplicate edges in same direction
-                    edges.add([{
-                        from: lastNode, to: ndId, color: getEdgeColor(1),
-                        level: 1, selectionWidth: 2, hoverWidth: 0,arrows:'to'
-                    }]);
-                }
-                lastNode = ndId;
-
-            }
+            addNodeAndEdge(paths[i][j], lastNode);
+            lastNode = paths[i][j];
         }
     }
+    addNodeAndEdge(startID, "");
+    //for (var i = 0; i < paths.length;i++) {
+    //    var lastNode = "";
+    //    for (var j = 0; j < paths[i].length; j++) {
+    //        var ndId = paths[i][j];
+    //        var ndName = apiproxy.getEntity(ndId)['name'];
+    //        var isAim = (ndId == startID);
+    //        if (lastNode == "") {
+    //            if (nodes.getIds().indexOf(ndId) == -1) {
+    //                startpages.push(ndId)
+    //                nodes.add([
+    //                    {
+    //                        id: ndId, label: wordwrap(decodeURIComponent(ndName), 20), value: 2, level: 0,
+    //                        color: getDefaultColor(0), x: 0, y: 0, fixed: true,parent: ndId, isDefault: true, isAim: isAim, isMethod: false
+    //                    } // Parent is self
+    //                ]);
+    //            }
+    //           lastNode = ndId;
+    //        } else {
+    //            startpages.push(ndId)
+    //            if (nodes.getIds().indexOf(ndId) == -1) {
+    //                startpages.push(ndId)
+    //                if (isAim) {
+    //                    nodes.add([
+    //                        {
+    //                            id: ndId, label: wordwrap(decodeURIComponent(ndName), 20), value: 2, level: 4,
+    //                            color: getAimColor(0), x: 300, y: 0, fixed: true,parent: lastNode, isDefault: false, isAim: true, isMethod: false
+    //                        } // Parent is self
+    //                    ]);
+    //                } else {
+    //                    nodes.add([
+    //                        {
+    //                            id: ndId, label: wordwrap(decodeURIComponent(ndName), 20), value: 2, level: 4,
+    //                            color: getColor(0), x: 0, y: 0, parent: lastNode, isDefault: false, isAim: false, isMethod: false
+    //                        } // Parent is self
+    //                    ]);
+    //                }
+
+    //            }
+    //            if (!getEdgeConnecting(lastNode, ndId) && lastNode!=ndId) { //Don't create duplicate edges in same direction
+    //                edges.add([{
+    //                    id: lastNode + "_" + ndId,
+    //                    from: lastNode, to: ndId, color: getEdgeColor(1),
+    //                    level: 1, selectionWidth: 2, hoverWidth: 0,arrows:'to'
+    //                }]);
+    //            }
+    //            lastNode = ndId;
+
+    //        }
+    //    }
+    //}
     data = { nodes: nodes, edges: edges };
     network.setData(data);
 }
- 
 
-  // Change "go" button to a refresh icon
- 
 
-  // -- CREATE NETWORK -- //
-  //Make a container
-  
-  //Put the data in the container
-  
+function addNodeAndEdge(nodeId, parentId) {
+    // Default entity
+    daId = apiproxy.getDefaultEntity()['id']
+    if (nodeId == daId) {
+        if (nodes.getIds().indexOf(nodeId) == -1) {
+            startpages.push(nodeId)
+            var nodeName = apiproxy.getEntity(nodeId)['name'];
+            nodes.add([
+                {
+                    parents: [],
+                    id: nodeId, label: wordwrap(decodeURIComponent(nodeName), 20), value: 2, level: 0,
+                    color: getDefaultColor(0), x: 0, y: 0, fixed: true, parent: nodeId, isDefault: true, isAim: false, isMethod: false
+                } // Parent is self
+            ]);
+        }
+    } else {
+
+        if (nodes.getIds().indexOf(nodeId) == -1) {
+            // new path
+            startpages.push(nodeId)
+            var nodeName = apiproxy.getEntity(nodeId)['name'];
+            nodes.add([
+                {
+                    parents: [parentId],
+                    id: nodeId, label: wordwrap(decodeURIComponent(nodeName), 20), value: 2, level: 0,
+                    color: getColor(0), x: 0, y: 0, parent: parentId, isDefault: false, isAim: false, isMethod: false
+                }
+            ]);
+        } else {
+            // another path
+            var nodeName = apiproxy.getEntity(nodeId)['name'];
+            parents = nodes.get(nodeId).parents;
+            parents.push(parentId);
+            nodes.update({ id: nodeId, parents: parents });
+            
+        }
+
+        if (!getEdgeConnecting(parentId, nodeId) && parentId != nodeId) { //Don't create duplicate edges in same direction
+            edges.add([{
+                id: getEdgeId(parentId, nodeId),
+                from: parentId, to: nodeId, color: getEdgeColor(1),
+                level: 1, selectionWidth: 2, hoverWidth: 0, arrows: 'to'
+            }]);
+        }
+
+    }
+}
+
+function getEdgeId(parentId, nodeId) {
+    return parentId + "_" + nodeId;
+}
+
+
+// Change "go" button to a refresh icon
+
+
+// -- CREATE NETWORK -- //
+//Make a container
+
+//Put the data in the container
+
 
 
 // Add a new start node to the map.
 function addStart(start, url) {
-  var name = apiproxy.getEntity(start)['name'];
-  
-  if (needsreset) {
-    // Delete everything only for the first call to addStart by tracking needsreset
-      resetNetwork(start, name);
-    needsreset = false;
-    return;
+    var name = apiproxy.getEntity(start)['name'];
 
-  } else {
-      var startID = getNeutralId(start);
-      //console.log(startID);
-    startpages.push(startID);
-    nodes.add([
-      {id:startID, label:wordwrap(decodeURIComponent(name),20), value:2, level:0,
-            color: getColor(0), x: 0, y: 0, parent: startID, isDefault: false, isAim: (ndId == startID),isMethod:false, isExpand: false} // Parent is self
-    ]);
-  }
+    if (needsreset) {
+        // Delete everything only for the first call to addStart by tracking needsreset
+        resetNetwork(start, name);
+        needsreset = false;
+        return;
+
+    } else {
+        var startID = getNeutralId(start);
+        //console.log(startID);
+        startpages.push(startID);
+        nodes.add([
+            {
+                id: startID, label: wordwrap(decodeURIComponent(name), 20), value: 2, level: 0,
+                color: getColor(0), x: 0, y: 0, parent: startID, isDefault: false, isAim: (ndId == startID), isMethod: false, isExpand: false
+            } // Parent is self
+        ]);
+    }
 }
 
 
 // Reset the network with the content from the input box.
 function resetNetworkFromInput() {
-  // Network should be reset
-  needsreset = true;
-  var cf = document.getElementsByClassName("commafield")[0];
-  // Items entered.
+    // Network should be reset
+    needsreset = true;
+    var cf = document.getElementsByClassName("commafield")[0];
+    // Items entered.
     var inputs = getItems(cf);
-  // If no input is given, prompt user to enter articles
-  if (!inputs[0]) {
-    noInputDetected();
-    return;
-  }
+    // If no input is given, prompt user to enter articles
+    if (!inputs[0]) {
+        //noInputDetected();
+        return;
+    }
 
 
-  for (var i=0; i<1; i++) {
-    getPageName(encodeURI(inputs[i]), addStart);
-  }
+    for (var i = 0; i < 1; i++) {
+        getPageName(encodeURI(inputs[i]), addStart);
+    }
 }
 
 // Reset the network with the content from the input box.
@@ -168,26 +232,26 @@ function resetNetworkFromInputDefault() {
 
 // Reset the network with one or more random pages.
 function randomReset() {
-  needsreset = true;
-  clearItems(cf);
-  // Function to add a single random page to the network as a start.
+    needsreset = true;
+    clearItems(cf);
+    // Function to add a single random page to the network as a start.
     resetNetworkFromInputDefault();
- 
+
 }
 
 // Reset the network with content from a JSON string
 function resetNetworkFromJson(data) {
-  if (!initialized) makeNetwork();
-  var obj = networkFromJson(data);
-  nodes = obj.nodes;
-  edges = obj.edges;
-  startpages = obj.startpages;
-  // Fill the network
-  network.setData({nodes:nodes, edges:edges});
-  // Populate the top bar
-  for (var i=0; i<startpages.length; i++) {
-    addItem(document.getElementById("input"), nodes.get(startpages[i]).label);
-  }
-  // Transform the "go" button to a "refresh" button
-  document.getElementById("submit").innerHTML = '<i class="icon ion-refresh"> </i>';
+    if (!initialized) makeNetwork();
+    var obj = networkFromJson(data);
+    nodes = obj.nodes;
+    edges = obj.edges;
+    startpages = obj.startpages;
+    // Fill the network
+    network.setData({ nodes: nodes, edges: edges });
+    // Populate the top bar
+    for (var i = 0; i < startpages.length; i++) {
+        addItem(document.getElementById("input"), nodes.get(startpages[i]).label);
+    }
+    // Transform the "go" button to a "refresh" button
+    document.getElementById("submit").innerHTML = '<i class="icon ion-refresh"> </i>';
 }
